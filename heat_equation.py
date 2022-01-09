8dcbae4a8faab2fceaae60b52aaf043f3c63ed80
subhamkd <subhamkdas98@gmail.com>
2022-01-09 16:33:02 -0700

    added notebook tqdm bars

--- heat_equation.py
+++ heat_equation.py
@@ -1,6 +1,6 @@
 #solving the heat equation using finite difference method
 import numpy as np
-from tqdm import tqdm
+from tqdm.notebook import tqdm_notebook
 
 def heatBC(a,b,c,d,T,dx,dy):
     
@@ -41,7 +41,7 @@ def heatBC(a,b,c,d,T,dx,dy):
 
 def heat_equation(T,dx,dy,alpha,dt,nt,TBCs):
     Tn = np.empty_like(T)
-    for n in tqdm(range(nt)):
+    for n in tqdm_notebook(range(nt)):
         Tn = T.copy()
         T[1:-1,1:-1]=Tn[1:-1,1:-1]+(alpha*dt/dx**2)*(Tn[2:,1:-1]-2*Tn[1:-1,1:-1]+Tn[0:-2,1:-1])+(alpha*dt/dy**2)*(Tn[1:-1,2:]- 2 * Tn[1:-1, 1:-1] + Tn[1:-1, 0:-2])
         
